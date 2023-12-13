1441c9448a07328abdbf23b1b98aa07878b531fa
snakemanser <137651357+snakemanser@users.noreply.github.com>
2023-12-14 02:12:54 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
 URL_SERVICE = "https://f44ad6f5-fae7-4742-b97b-a9ceebeb15d7.serverhub.praktikum-services.ru"
 DOC_PATH = "/docs/"
 CREATE_ORDER_PATH = "/api/v1/orders"
-ORDER_BY_TRACK = "/api/v1/orders/track"
+ORDER_BY_TRACK = "/api/v1/orders/track?t="
