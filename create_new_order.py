6aadbf0e9c7c6aee49393d4bc55696041dce463f
snakemanser <137651357+snakemanser@users.noreply.github.com>
2023-12-14 02:11:11 +0300

    Update create_new_order.py

--- create_new_order.py
+++ create_new_order.py
@@ -2,6 +2,7 @@ import configuration
 import requests
 import data
 
+
 # Шмуляев Виталий 11-когорта  — Финальный проект. Инженер по тестированию плюс
 
 def post_new_order(body):
@@ -10,24 +11,13 @@ def post_new_order(body):
                          headers=data.headers)
 
 
-response = post_new_order(data.user_body);
-print(response.status_code)
-print(response.json())
-
-track = response.json()['track']
-query_track = {
-    "t": track
-}
+track = str(post_new_order(data.order_body).json()['track'])
 
 
-def get_order_by_track():
-    return requests.get(configuration.URL_SERVICE + configuration.ORDER_BY_TRACK, params=query_track)
-
-
-response = get_order_by_track();
-print(response.status_code)
-print(response.json())
+def get_order_by_track(track_id):
+    return requests.get(configuration.URL_SERVICE + configuration.ORDER_BY_TRACK + track_id)
 
 
 def test_status_code():
+    response = get_order_by_track(track)
     assert response.status_code == 200
